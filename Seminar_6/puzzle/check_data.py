986d23cc12882682d4aacb12c2f7f8a272ce0c07
Oxana <crazyglass@list.ru>
2023-07-31 19:06:46 +0300

    Семинар 6

--- Seminar_6/puzzle/check_data.py
+++ Seminar_6/puzzle/check_data.py
@@ -9,6 +9,18 @@
 # Проверку года на високосность вынести в отдельную 
 # защищённую функцию.
 
+# В модуль с проверкой даты добавьте возможность запуска в 
+# терминале с передачей даты на проверку.
+
+__all__ = ['check']
+
+YEARS = range(1, 10000)
+MONTHS = range(1, 13)
+DAYS = range(1, 32)
+LIST_30 = [4, 6, 9, 11]
+DAY_30 = 30
+VIS_MONTH = 2
+DAY_VIS = 29
 
 def _check_vis(year: int) -> bool:
     CHECK_NORMAL_1 = 4
@@ -18,5 +30,26 @@ def _check_vis(year: int) -> bool:
         return True
     return False
 
+
 def check(data:str) -> bool:
-    pass
+    list_data = list(int(i) for i in data.split('.'))
+    if list_data[0] in DAYS and list_data[1] in MONTHS and list_data[2] in YEARS:
+        if list_data[0] > DAY_30 and list_data[1] in LIST_30:
+            return False
+        if list_data[0] > DAY_VIS  and list_data[1] == VIS_MONTH:
+            return False
+        if list_data[0] == DAY_VIS  and list_data[1] == VIS_MONTH and not _check_vis(list_data[2]):
+            return False
+        return True
+    else:
+        return False
+    
+
+if __name__ == '__main__':
+    print(check('40.02.2020'))
+    print(check('10.25.2020'))
+    print(check('31.02.2020'))
+    print(check('30.04.2020'))
+    print(check('31.04.2020'))
+    print(check('29.02.2020'))
+    print(check('29.02.2021'))
